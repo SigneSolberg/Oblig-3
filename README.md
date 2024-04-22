8b3473fb3cc0c03490a5e7c7aaafa66c55e993dd
SigneSolberg <143514951+SigneSolberg@users.noreply.github.com>
2024-04-22 17:17:00 +0200

    Justeringer og endringer i kode. Fått valideringen til å fungere riktig.

--- README.md
+++ README.md
@@ -7,9 +7,12 @@ Github repo URL: https://github.com/SigneSolberg/Oblig-2.git
 
 Fullt navn: Signe Solberg Jørgensen
 
-Kort beskrivelse av applikasjon: 
-Har endret lagring til database. 
+Kort beskrivelse av applikasjon:
 Har laget en applikajson for kjøp og salg av kinobilletter.
 Man kan velge film og skrive inn person informasjon samt 
 se sine kjøpte billetter og slette alle billettene. Dette er en utvidelse fra oblig 1 og 
 2, hvor arrayet fra tjener/server er erstattet med en tabell i relasjonsdatabase.
+
+Link til video: 
+
+
--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -39,101 +39,95 @@ function regKjøpeBillett() {
         epost: epost,
     };
 
-    //sender billetter til serveren for å lagre billettene
-    $.post("/lagreBillett", Billett, function () {
-        viseKjøpteBilletter();
-
-    });
-
     let valideringFeil = false;
 
     //Her har jeg if-setninger (for å få frem feilmeldinger) for antall, fornavn,etternavn,telefonnr og epost.
     if (antall <= 0 || isNaN(antall)) {
-        valideringFeil = true;
         $("#feilAntall").html("feil, skriv inn et heltall");
-    }
-    if (fornavn || fornavn.length === 0 || !isNaN(fornavn)) {
         valideringFeil = true;
+    }
+    if (fornavn.length === 0 || !isNaN(fornavn)) {
         $("#feilFornavn").html("Feil skrevet, skriv KUN med bokstaver");
-
-    }
-    if (etternavn || etternavn.length === 0 || !isNaN(etternavn)) {
         valideringFeil = true;
-        $("#feilEtternavn").html("Feil etternavn, skriv Kun med bokstaver");
-
     }
-    if (telefonnr || telefonnr.length !== 8 || isNaN(telefonnr)) {
+    if (etternavn.length === 0 || !isNaN(etternavn)) {
+        $("#feilEtternavn").html("Feil etternavn, skriv KUN med bokstaver");
         valideringFeil = true;
-        $("#feilTelefonnr").html("Feil telefonNr, skriv et telefonnr med 8 siffer");
-
+    }
+    if (telefonnr.length !== 8 || isNaN(telefonnr)) {
+        $("#feilTelefonnr").html("Feil telefonnr, skriv et telefonnr med 8 siffer");
+        valideringFeil = true;
     }
     //Her har jeg brukt en REGEX for validering av epost
     if (!/\S+@\S+\.\S+/.test(epost)) {
-        valideringFeil = true;
         $("#feilEpost").html("Feil epost, prøv på nytt");
-
+        valideringFeil = true;
     }
-    // Tømme og fjerne feilmeldingene når man får kjøpt billett og brukeren har rettet opp i feil
-    $("#feilAntall").html("");
-    $("#feilFornavn").html("");
-    $("#feilEtternavn").html("");
-    $("#feilTelefonnr").html("");
-    $("#feilEpost").html("");
 
     // Hvis valideringen er vellykket vil billettene lagres
     if (!valideringFeil) {
-        regKjøpeBillett()
-    }
+        //sender billetter til serveren for å lagre billettene
+        $.post("/lagreBillett", Billett, function () {
+            viseKjøpteBilletter();
 
-    //tømme arrayet/slette info fra input boksene
-    $("#film").val("");
-    $("#antall").val("");
-    $("#fornavn").val("");
-    $("#etternavn").val("");
-    $("#telefonnr").val("");
-    $("#epost").val("");
-
-    // funksjon for å vise og hente de kjøpte billettene
-    function viseKjøpteBilletter() {
-        $.get("/hentAlleBilletter", function (KjøpteBilletter) {
-            opprettBilletter(KjøpteBilletter);
         });
-    }
+        // Tømme og fjerne feilmeldingene når man får kjøpt billett og brukeren har rettet opp i feil
+        $("#feilAntall").html("");
+        $("#feilFornavn").html("");
+        $("#feilEtternavn").html("");
+        $("#feilTelefonnr").html("");
+        $("#feilEpost").html("");
 
-    //funksjon for å opprettelse av array og formatering av billetter
-    function opprettBilletter(KjøpteBilletter) {
-        let ut = "<table><tr>" +
-            "<th>Film</th><th>Antall</th>" +
-            "<th>Fornavn</th><th>Etternavn</th><th>Telefonnr</th><th>Epost</th>"
-            + "</tr>";
-        for (let k of KjøpteBilletter) {
-            ut += "<tr>";
-            ut += "<td>" +
-                k.film + "</td><td>" + k.antall
-                + "</td><td>" + k.fornavn + "</td><td>" + k.etternavn + "</td><td>"
-                + k.telefonnr + "</td><td>" + k.epost + "</td>";
-            ut += "</tr>";
-        }
-        ut += "</table>";
-        $("#Kjøptebilletter").html(ut);
+        //tømme arrayet/slette info fra input boksene
+        $("#film").val("");
+        $("#antall").val("");
+        $("#fornavn").val("");
+        $("#etternavn").val("");
+        $("#telefonnr").val("");
+        $("#epost").val("");
     }
+}
+        // funksjon for å vise og hente de kjøpte billettene
+        function viseKjøpteBilletter() {
+            $.get("/hentAlleBilletter", function (KjøpteBilletter) {
+                opprettBilletter(KjøpteBilletter);
+            });
+        }
+
+        //funksjon for å opprettelse av array og formatering av billetter
+        function opprettBilletter(KjøpteBilletter) {
+            let ut = "<table><tr>" +
+                "<th>Film</th><th>Antall</th>" +
+                "<th>Fornavn</th><th>Etternavn</th><th>Telefonnr</th><th>Epost</th>"
+                + "</tr>";
+            for (let k of KjøpteBilletter) {
+                ut += "<tr>";
+                ut += "<td>" +
+                    k.film + "</td><td>" + k.antall
+                    + "</td><td>" + k.fornavn + "</td><td>" + k.etternavn + "</td><td>"
+                    + k.telefonnr + "</td><td>" + k.epost + "</td>";
+                ut += "</tr>";
+            }
+            ut += "</table>";
+            $("#KjøpteBilletter").html(ut);
+        }
 
 //lager en funksjon for å vise og hente opp de kjøpte billettene
-function hentAlleBilletter() {
-    $.get("/hentAlleBilletter", function (data) {
-        opprettBilletter(data);
-    });
-}
-$(document).ready(function () {
-    hentAlleBilletter();
-});
-}
-//Oppretter en funksjon for å slette de kjøpte billettene ved at man trykker på (slett alle billetter)
-    function slettAlleBilletter() {
-        $.get("/slettAlleBilletter", function () {
-            regKjøpeBillett();
+            function hentAlleBilletter() {
+                $.get("/hentAlleBilletter", function (data) {
+                    opprettBilletter(data);
+                })
+            }
+            $(document).ready(function () {
+                hentAlleBilletter();
         });
-    }
+
+//Oppretter en funksjon for å slette de kjøpte billettene ved at man trykker på (slett alle billetter)
+        function slettAlleBilletter() {
+            $.get("/slettAlleBilletter", function () {
+                viseKjøpteBilletter();
+            });
+        }
 
 
 
